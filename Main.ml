82865deeab72210750e5b9d6191636e5adf5d0a2
Serena Wang <lutongwang@college.harvard.edu>
2014-04-19 23:39:03 -0400

    OMG DIJKSTRA'S WORKING (dist only, no prev, I don't know how to print those)

--- Main.ml
+++ Main.ml
@@ -3,10 +3,14 @@ open Dict
 open Set
 open Prioqueue
 open Graph
+open Order
 
-let graph = NamedGraph.from_edges [("a","b", 2.); ("b","c",1.);("b","a",2.)];;
+let cs124graph = NamedGraph.from_edges 
+		   [("s","a", 2.); ("a","c",1.);("c","e",4.);("s","b",6.);
+		    ("b","d",2.);("d","f",2.);("f","e",1.);("c","f",2.);
+		    ("c","b",1.);("a","b",3.);("b","a",5.)];;
 
-print_string (let hd::_ = (NamedGraph.nodes graph) in hd);
+(*  print_string (let hd::_ = (NamedGraph.nodes graph) in hd); *)
 (*
 let cmdargs = Array.to_list Sys.argv in
      Dijkstra's algorithm *)
@@ -15,8 +19,8 @@ module DistDict = Dict.Make(
   struct
     type key = string
     type value = float
-    let compare = Int.compare
-    let string_of_key = key
+    let compare = string_compare
+    let string_of_key x = x
     let string_of_value = Float.to_string
   end)
 
@@ -24,9 +28,9 @@ module PrevDict = Dict.Make(
   struct
     type key = string
     type value = string
-    let compare = String.compare
-    let string_of_key = key
-    let string_of_value = value
+    let compare = string_compare
+    let string_of_key x = x
+    let string_of_value x = x
   end)
 
 module NodeHeapQueue = (BinaryHeap(PtCompare) :
@@ -34,38 +38,49 @@ module NodeHeapQueue = (BinaryHeap(PtCompare) :
 
 
 let dijkstra (graph: NamedGraph.graph) (s: NamedGraph.node) (fin: NamedGraph.node) 
-    : DistDict.dict * PrevDict.dict =
-  let rec helper (H: NodeHeapQueue.queue) (dist: DistDict.dict) (prev: PrevDict.dict) 
+    : float =
+  let rec helper (heap: NodeHeapQueue.queue) (dist: DistDict.dict) 
+		 (prev: PrevDict.dict) 
           : DistDict.dict * PrevDict.dict =
-    if NodeHeapQueue.is_empty H then (dist,prev)
+    if NodeHeapQueue.is_empty heap then (dist,prev)
     else 
-      let (v, H') = NodeHeapQueue.take H in
-      match NamedGraph.neighbors graph v with
+      let ((v_node,v_dist), heap') = NodeHeapQueue.take heap in
+      match NamedGraph.neighbors graph v_node with
       | None -> failwith "Neighborless Node"
       | Some lst ->
 	 let (newheap, newdist, newprev) = 
-           List.fold_right lst 
-			   ~f:(fun w (h,d,p) -> 
-			       let (w',length) = w in
-			       match DistDict.lookup d w' with 
-			       | None -> failwith "Node not inserted"
-			       | Some distw -> 
-				  let distw' = (DistDict.lookup dist v) + length in
-				  if distw' < distw then
-				    let h' = NodeHeapQueue.add (w', distw') h in
-				    let d' = DistDict.insert d w' distw' in
-				    let p' = PrevDict.insert p w' v in
-				    (h', d', p')
-				  else (h,d,p))
-			   ~init: (H',dist,prev) in 
+           List.fold_right 
+	     lst 
+	     ~f:(fun w (h,d,p) -> 
+		 let (w_node,w_length) = w in
+		 match (DistDict.lookup d w_node), 
+		       (DistDict.lookup dist v_node) with 
+		 | None, _ -> failwith "that shouldn't happen"
+		 | _, None -> failwith "that shouldn't happen"
+		 | Some distw, Some distv -> 
+		    let distw' = distv +. w_length in
+		    if distw' < distw then
+		      let h' = NodeHeapQueue.add (w_node, distw') h in
+		      let d' = DistDict.insert d w_node distw' in
+		      let p' = PrevDict.insert p w_node v_node in
+		      (h', d', p')
+		    else (h,d,p))
+	     ~init: (heap',dist,prev) in 
 	 helper newheap newdist newprev in
-		   
-  let initial_heap = (NodeHeapQueue.add (s,0.) empty) in
-  let initial_dist = List.fold_right (NamedGraph.nodes graph) 
-				     ~f:(fun n d -> DistDict.insert d n Int.max_value) 
-				     ~init:DistDict.empty in
-  let initial_prev = PrevDict.empty in
-						       
-  helper initial_heap initial_dist initial_prev
+  let initial_heap = (NodeHeapQueue.add (s,0.) NodeHeapQueue.empty) in
+  let initial_dist_before = List.fold_right 
+		       (NamedGraph.nodes graph) 
+		       ~f:(fun n d -> DistDict.insert d n Float.max_value) 
+		       ~init:DistDict.empty in
+  let initial_dist_updated = (DistDict.insert initial_dist_before s 0.) in
+  let initial_prev = PrevDict.empty in						    
+  let (final_dist,final_prev) = (helper initial_heap initial_dist_updated
+					initial_prev) in
+  match DistDict.lookup final_dist fin with
+  | None -> Float.max_value
+  | Some n -> n
+;;
+  
     
    
+Printf.printf "%f" (dijkstra cs124graph "s" "a");;
